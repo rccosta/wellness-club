31e6542790d6ad16cda6299438288c2c8342a873
Rodrigo Costa <rccosta.webdesign@gmail.com>
2019-04-28 18:33:49 -0300

    Adicionado arquivo formularios

--- view/captar/inc/footer.php
+++ view/captar/inc/footer.php
@@ -1,4 +1,19 @@
-
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <small>© copyright - Wellness Club 2019 - Todos os direitos reservados</small>
+            </div>
+            <div class="col-md-4">
+                <ul class="socialmedia">
+                <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                <li><a href="#" target="_blank" ><i class="fab fa-facebook-square"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 <script type="text/javascript" src="<?php echo _URL_SISTEMA; ?>view/vendor/bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo _URL_SISTEMA; ?>view/vendor/jquery/js/jquery.min.js"></script>
 </body>
