ff804a01d313f0326acc5e97a3254aebaf81db35
Rodrigo Costa <rccosta.webdesign@gmail.com>
2019-05-17 17:36:09 -0300

    Campo de noticias
    Barra lateral de noticias

---
+++ view/build/templates/noticias.php
@@ -0,0 +1,38 @@
+<div id="quickviewDefault" class="quickview">
+  <div class="quickview-div">   
+    <span class="delete" data-dismiss="quickview"></span>
+    <div class="is-divider" data-content="Últimas notícias"></div>
+  </div>
+  
+    <div class="timeline">
+    <div class="timeline-header">
+    <span class="tag is-medium is-outlined">Ínicio</span>
+    </div>
+
+    <div class="corpo-noticias">
+    <!-- Item -->
+    <div class="timeline-item">
+        <div class="timeline-marker is-icon">
+            <i class="fa fa-flag"></i>
+        </div>
+        <div class="timeline-content">
+            <p class="heading">Novo Produto</p>
+            <small>Publicado 12/05/2019</small>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam asperiores cumque doloremque voluptas aut totam modi corrupti optio quidem rem officiis sunt hic deleniti vel, veniam deserunt repellendus facere harum?</p>
+            
+        </div>
+    </div>
+    <!-- Final item -->
+
+
+ 
+
+
+    <div class="timeline-header">
+        <span class="tag is-medium is-outlined">Final</span>
+    </div>
+
+    </div>
+ 
+</div>
+</div>
